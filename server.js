7ef9b26d728952accf99124034e2145e21cea6ad
Jide Adesanya <Jide1012@yahoo.com>
2022-05-18 14:08:55 -0500

    added routes and funtionality to the js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({extended:true}))
+
+//Make files static
+app.use(express.static('public'));
+
+//
+app.use(express.json());
+
+
+app.listen(PORT, () =>{
+    console.log('API server now on port ${PORT}');
+}
+
+)
